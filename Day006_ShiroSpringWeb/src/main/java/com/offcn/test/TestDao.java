cbda48415382ab2793c80a783302c99f70697b6f
haofenglei <1143064564@qq.ocm>
2018-03-08 20:32:34 +0800

    修改了

--- Day006_ShiroSpringWeb/src/main/java/com/offcn/test/TestDao.java
+++ Day006_ShiroSpringWeb/src/main/java/com/offcn/test/TestDao.java
@@ -13,7 +13,7 @@ public class TestDao {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-dao.xml");
 		RoleMapper roledao=(RoleMapper) context.getBean("roleMapper");
-		
+		//sagdhsadasj
 		List<Role> rolelist = roledao.findRoleList("admin");
 		for (Role role : rolelist) {
 			System.out.println(role.getRoleName()+" "+role.getRoleSign());
